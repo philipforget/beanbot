79888cdd159d6db1d85ed41e14b7cd62d419f5de
Philip Forget <philipforget@gmail.com>
2012-06-20 21:26:46 -0400

    track coffee project

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer();
 
-app.use(express.static(__dirname));
-app.set('views', __dirname)
-
 app.get('/', function(req, res){
-    res.render('index.jade', {layout: false});
-});
+    res.send('Fuck');
+})
+
+app.listen(3000);
