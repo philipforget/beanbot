8104819677d67760445bac0e3e79a12082c0f6cd
Philip Forget <philipforget@gmail.com>
2012-06-20 21:27:31 -0400

    cleanup

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express.createServer();
-
-app.get('/', function(req, res){
-    res.send('Fuck');
-})
-
-app.listen(3000);
--- beanbot.coffee
+++
@@ -1,3 +0,0 @@
-#! ./node_modules/coffee-script/bin/coffee
-
-LM75A = require('LM75A').LM75A
